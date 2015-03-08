9dc6c7a8316b1cb4031583a6737c657be9a3893a
SpencerKunz <spener.kunz25@gmail.com>
2015-03-08 14:09:11 -0500

    Update 2-1.py

--- Problem-Set-2/2-1.py
+++ Problem-Set-2/2-1.py
@@ -1,22 +1,37 @@
-from datetime import datetime
-import time
+import pandas
+import pandasql
 
-def reformat_subway_dates(date):
-    '''
-    The dates in our subway data are formatted in the format month-day-year.
-    The dates in our weather underground data are formatted year-month-day.
-    
-    In order to join these two data sets together, we'll want the dates formatted
-    the same way.  Write a function that takes as its input a date in the MTA Subway
-    data format, and returns a date in the weather underground format.
-    
-    Hint: 
-    There is a useful function in the datetime library called strptime. 
-    More info can be seen here:
-    http://docs.python.org/2/library/datetime.html#datetime.datetime.strptime
-    '''
 
-    t = time.strptime(date, '%m-%d-%y')
-    d = datetime(*t[:6])
-    date_formatted = d.strftime('%Y-%m-%d')
-    return date_formatted
+def num_rainy_days(filename):
+    '''
+    This function should run a SQL query on a dataframe of
+    weather data.  The SQL query should return one column and
+    one row - a count of the number of days in the dataframe where
+    the rain column is equal to 1 (i.e., the number of days it
+    rained).  The dataframe will be titled 'weather_data'. You'll
+    need to provide the SQL query.  You might find SQL's count function
+    useful for this exercise.  You can read more about it here:
+    
+    https://dev.mysql.com/doc/refman/5.1/en/counting-rows.html
+    
+    You might also find that interpreting numbers as integers or floats may not
+    work initially.  In order to get around this issue, it may be useful to cast
+    these numbers as integers.  This can be done by writing cast(column as integer).
+    So for example, if we wanted to cast the maxtempi column as an integer, we would actually
+    write something like where cast(maxtempi as integer) = 76, as opposed to simply 
+    where maxtempi = 76.
+    
+    You can see the weather data that we are passing in below:
+    https://www.dropbox.com/s/7sf0yqc9ykpq3w8/weather_underground.csv
+    '''
+    weather_data = pandas.read_csv(filename)
+
+    q = """
+     SELECT count(*)
+     FROM weather_data
+     WHERE cast(rain as integer) = 1;
+    """
+    
+    #Execute your SQL command against the pandas frame
+    rainy_days = pandasql.sqldf(q.lower(), locals())
+    return rainy_days
